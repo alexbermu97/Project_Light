331fc8c83144a626f2933c0fcb7f9c40164a1eae
whispfire <whispfire.studio@gmail.com>
2019-07-22 19:11:53 +0200

    - Creación del proyecto

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    Vector2 movDir;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //Mando
+        float hc = Input.GetAxis("HorizontalC");
+        float vc = Input.GetAxis("VerticalC");
+
+        movDir = (hc * transform.right + vc * transform.up);
+        rb.velocity = movDir * speed * Time.deltaTime;
+        //MandoFin
+    }
+}
