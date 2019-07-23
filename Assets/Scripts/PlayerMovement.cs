c7832782251f3f7ede4ef31d57526bd30c7e2139
whispfire <whispfire.studio@gmail.com>
2019-07-23 10:34:19 +0200

    - Actualización script PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //ESTE SCRIPT CONTROLA ÚNICAMENTE EL MOVIMIENTO DEL JUGADOR, TANTO CON MANDO COMO CON PANTALLA TÁCTIL
     public Rigidbody2D rb;
     public float speed;
     Vector2 movDir;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //BUSCA EN EL OBJETO QUE POSEA ESTE SCRIPT EL COMPONENTE RIGIDBODY2D Y LO ASIGNA A LA VARIABLE
     }
     
     void Update()
     {
-        Move();
+        ControllerMove();
     }
 
-    void Move()
+    void ControllerMove()
     {
         //Mando
         float hc = Input.GetAxis("HorizontalC");
         float vc = Input.GetAxis("VerticalC");
 
-        movDir = (hc * transform.right + vc * transform.up);
-        rb.velocity = movDir * speed * Time.deltaTime;
+        movDir = (hc * transform.right + vc * transform.up); //ACTUALIZA VECTOR MOVDIR
+        rb.velocity = movDir * speed * Time.deltaTime; //ACTUALIZA VECTOR VELOCITY, QUE SE APLICA AL RIGIDBODY2D Y PERMITE MOVIMIENTO
         //MandoFin
     }
 }
